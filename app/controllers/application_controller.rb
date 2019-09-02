e1f34a05b4ffa5aec517dc2167b5a1ce1d47a4dd
Nicole Dow <nikki.dow71@gmail.com>
2019-09-02 18:24:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do 
+    erb :new
+  end
+  
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params["id"])
+    erb :edit_recipe
+  end
+  
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params["id"])
+    erb :show
+  end
+  
+  post '/recipes' do
+    Recipe.create(:name => params["name"], :ingredients => params["ingredients"], :cook_time => params["cook_time"])
+    redirect to '/recipes'
+  end
+  
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params["id"])
+    @recipe.destroy
+    redirect to '/recipes'
+  end
+
+
+
+
 
 end
