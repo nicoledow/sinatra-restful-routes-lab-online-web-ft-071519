f3605cffd1af906836543cc85c6cfe1d809534ef
Nicole Dow <nikki.dow71@gmail.com>
2019-09-02 18:27:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/recipes' do
-    Recipe.create(:name => params["name"], :ingredients => params["ingredients"], :cook_time => params["cook_time"])
+    @recipe = Recipe.create(:name => params["name"], :ingredients => params["ingredients"], :cook_time => params["cook_time"])
     redirect to '/recipes'
   end
   
